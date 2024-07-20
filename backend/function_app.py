044cb284087a098bef26adff771ba0758ca9ff28
eladvengrover <116734033+eladvengrover@users.noreply.github.com>
2024-07-20 18:23:12 +0300

    Connected CreateNewBooking FunctionApp to DB and front(#3)

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,12 +1,14 @@
 import azure.functions as func
 import logging
+from db.operations import add_booking
+from datetime import datetime
 
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="CreateNewBooking")
 def CreateNewBooking(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request: CreateNewBooking.')
 
     name = req.params.get('name')
     if not name:
@@ -17,8 +19,6 @@ def CreateNewBooking(req: func.HttpRequest) -> func.HttpResponse:
             pass
     else:
         name = req_body.get('name')
-
-
 
     try:
         add_booking(
@@ -35,13 +35,6 @@ def CreateNewBooking(req: func.HttpRequest) -> func.HttpResponse:
         logging.error(f"Error in CreateNewBooking: {e}")
         return func.HttpResponse(f"Error: {str(e)}", status_code=500)
 
-    # if name:
-    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    # else:
-    #     return func.HttpResponse(
-    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-    #          status_code=200
-    #     )
 
 @app.route(route="RemoveBooking", auth_level=func.AuthLevel.ANONYMOUS)
 def RemoveBooking(req: func.HttpRequest) -> func.HttpResponse:
