0ec1d59e74514ebdbb8d53f6df9c21d28c047894
shahargraiver <116727253+shahargraiver@users.noreply.github.com>
2024-10-18 12:14:44 +0300

    fix merge of backend (#11)

--- backend/db/db_types/parking_table_types.py
+++ backend/db/db_types/parking_table_types.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String # type: ignore 
+from sqlalchemy import Column, Integer, String, Boolean # type: ignore 
 from sqlalchemy.ext.declarative import declarative_base # type: ignore
 
 Base = declarative_base()
@@ -10,4 +10,3 @@ class Parking(Base):
     location = Column(String)
     building_id = Column(Integer, nullable=False)
     is_permanently_blocked = Column(Boolean, default=False)
-
--- backend/db/parkings_operations.py
+++ backend/db/parkings_operations.py
@@ -1,3 +1,4 @@
+from booking_managment import remove_bookings_by_parking_id
 from .connection import session
 from db.db_types.parking_availability_types import ParkingAvailability
 from db.db_types.parking_table_types import Parking
@@ -7,6 +8,8 @@ from booking_managment import remove_bookings_by_parking_id
 from sqlalchemy import and_
 from sqlalchemy.orm.exc import NoResultFound
 import logging
+from sqlalchemy import and_ # type: ignore
+from sqlalchemy.orm.exc import NoResultFound # type: ignore
 
 
 
@@ -82,27 +85,6 @@ def get_parking_building_id(parking_id):
         logging.info(f"Error retrieving parking's building ID: {e}")
         return None
 
-def update_parking_details(parking_id, parking_number, location, building_id, is_permanently_blocked):
-    try:
-        existing_parking = session.query(Parking).filter_by(parking_id=parking_id).one()
-
-        # Part isn't relevant for current basic flow
-        # old_is_permanently_blocked_val = existing_parking.is_permanently_blocked
-
-        existing_parking.parking_id = parking_id
-        existing_parking.parking_number = parking_number
-        existing_parking.location = location
-        existing_parking.building_id = building_id
-        existing_parking.is_permanently_blocked = is_permanently_blocked
-        session.commit()
-
-        # Part isn't relevant for current basic flow
-        #if (old_is_permanently_blocked_val == False and is_permanently_blocked):
-            #remove_bookings_by_parking_id(parking_id)
-
-        return existing_parking.parking_id
-    except NoResultFound:
-        return -1
 
 def create_new_parking(parking_number, location, building_id, is_permanently_blocked):
     try:
@@ -128,19 +110,20 @@ def create_new_parking(parking_number, location, building_id, is_permanently_blo
         session.rollback()
         print(f"An error occurred: {e}")
         return -1
-    
+    is_permanently_blocked = Column(Boolean, default=False)
+
+
 
 def remove_parking(parking_id):
     try:
         # Check if the username exists
         existing_parking = session.query(Parking).filter_by(parking_id=parking_id).first()
-        
+
         if not existing_parking:
             print(f"Parking ID '{parking_id}' doesn't exist. Please choose a different parking.")
             return -1        
         session.delete(existing_parking)
         session.commit()
-
         remove_bookings_by_parking_id(parking_id)
         print(f"Parking '{parking_id}' has been removed")
 
@@ -150,11 +133,37 @@ def remove_parking(parking_id):
             return -1        
         session.delete(existing_parking)
         session.commit()
-
         return parking_id
-    
+   
     except Exception as e:
         session.rollback()
         print(f"An error occurred: {e}")
+        return -1
+
+
+def update_parking_details(parking_id, parking_number, location, building_id, is_permanently_blocked):
+    try:
+        existing_parking = session.query(Parking).filter_by(parking_id=parking_id).one()
+
+
+        # Part isn't relevant for current basic flow
+        # old_is_permanently_blocked_val = existing_parking.is_permanently_blocked
+
+
+        existing_parking.parking_id = parking_id
+        existing_parking.parking_number = parking_number
+        existing_parking.location = location
+        existing_parking.building_id = building_id
+        existing_parking.is_permanently_blocked = is_permanently_blocked
+        session.commit()
+
+
+        # Part isn't relevant for current basic flow
+        #if (old_is_permanently_blocked_val == False and is_permanently_blocked):
+            #remove_bookings_by_parking_id(parking_id)
+
+
+        return existing_parking.parking_id
+    except NoResultFound:
         return -1
 
